68b2cfc08c91cd59dd153d314f5f83ec235dac6b
Bruno <bruno.carnavalli_compasso@terrainvestimentos.com.br>
2023-06-21 14:44:01 -0300

    Adapted scripts for Array

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const bulbImage = document.querySelector('#bulbImage')
+const htmlArray = document.getElementById('main-array')
 
-function turnLightOn() {
-  bulbImage.src = './assets/images/pic_bulbon.gif'
+var characterField = document.querySelector('#characterField')
+
+var charactersArray = ["Luke", "Leia", "Han Solo"]
+
+htmlArray.innerHTML = charactersArray
+
+function addCharacters() {
+  charactersArray.push(characterField.value)
+
+  htmlArray.innerHTML = charactersArray
+  
+  clearField()
 }
 
-function turnLightOff() {
-  bulbImage.src = './assets/images/pic_bulboff.gif'
+function deleteCharacters() {
+  const index = charactersArray.indexOf(characterField.value)
+  if (index == -1) {
+    return
+  }
+
+  console.log(index)
+  charactersArray.splice(index, 1)
+
+  htmlArray.innerHTML = charactersArray
+
+  clearField()
+}
+
+function clearField() {
+  characterField.value = ''
 }
